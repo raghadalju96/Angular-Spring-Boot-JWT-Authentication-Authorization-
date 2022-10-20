3e4a839483535d9ee384e2c3336bc4911c2c2088
Raghad Aljuaid <47305353+raghadalju96@users.noreply.github.com>
2022-10-20 11:54:38 +0300

    backend done

---
+++ springboot-backend/springboot-backend/src/main/java/net/javaguides/springboot/security/WebSecurityConfig.java
@@ -0,0 +1,81 @@
+package net.javaguides.springboot.security;
+
+import net.javaguides.springboot.security.jwt.AuthEntryPointJwt;
+import net.javaguides.springboot.security.jwt.AuthTokenFilter;
+import net.javaguides.springboot.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+    @EnableWebSecurity
+    @EnableGlobalMethodSecurity(
+            // securedEnabled = true,
+            // jsr250Enabled = true,
+            prePostEnabled = true)
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Autowired
+        UserDetailsServiceImpl userDetailsService;
+
+        @Autowired
+        private AuthEntryPointJwt unauthorizedHandler;
+
+        @Bean
+        public AuthTokenFilter authenticationJwtTokenFilter() {
+            return new AuthTokenFilter();
+        }
+
+        @Override
+        public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+            authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        }
+
+        @Bean
+        @Override
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.cors().and().csrf().disable()
+                    .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                    .antMatchers("/api/test/**").permitAll()
+                    .anyRequest().authenticated();
+
+            http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+//                    Authentication authentication =
+//                    authenticationManager.authenticate(
+//                            new UsernamePasswordAuthenticationToken(username, password)
+//                    );
+//
+//            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+//// userDetails.getUsername()
+//// userDetails.getPassword()
+// userDetails.getAuthorities()
+        }
+
+}
+
+
